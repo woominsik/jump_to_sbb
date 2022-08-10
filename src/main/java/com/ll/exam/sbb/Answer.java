21189cc4762e17dad115b6caafa04d72440b744c
woominsik <9737638@naver.com>
2022-08-10 11:39:04 +0900

    Answer 객체 생성

---
+++ src/main/java/com/ll/exam/sbb/Answer.java
@@ -0,0 +1,26 @@
+package com.ll.exam.sbb;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import com.ll.exam.sbb.Question;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -1,22 +1,35 @@
 package com.ll.exam.sbb;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
-@Entity // 아래 Question 클래스는 엔티티 클래스이다.
-// 아래 클래스와 1:1로 매칭되는 테이블이 DB에 없다면, 자동으로 생성되어야 한다.
+@Entity
 public class Question {
-    @Id // primary key
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(length = 200) // varchar(200)
+
+    @Column(length = 200)
     private String subject;
+
     @Column(columnDefinition = "TEXT")
     private String content;
+
     private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
