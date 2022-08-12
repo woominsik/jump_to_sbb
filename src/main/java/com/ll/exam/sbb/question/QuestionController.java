5f82f9b806ba54814012915e7f594323b4e4b25c
woominsik <9737638@naver.com>
2022-08-12 14:45:54 +0900

    question_list.html 뷰 파일 추가

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -2,13 +2,12 @@ package com.ll.exam.sbb.question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuestionController {
     @RequestMapping("/question/list")
-    @ResponseBody
+    // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
     public String list() {
-        return "question list";
+        return "question_list";
     }
 }
