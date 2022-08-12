97fe93906ac4649cd63ba8eb28f90e97e899fe07
woominsik <9737638@naver.com>
2022-08-12 14:16:03 +0900

    양방향 관계 만들어주기

--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -1,31 +1,27 @@
 package com.ll.exam.sbb;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import javax.persistence.*;
-
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
-@Entity
-@ToString
+@Entity // 아래 Question 클래스는 엔티티 클래스이다.
+// 아래 클래스와 1:1로 매칭되는 테이블이 DB에 없다면, 자동으로 생성되어야 한다.
 public class Question {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
     private Integer id;
-
-    @Column(length = 200)
+    @Column(length = 200) // varchar(200)
     private String subject;
-
     @Column(columnDefinition = "TEXT")
     private String content;
-
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
-    private List<Answer> answerList;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList = new ArrayList<>();
 }
--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -36,6 +36,7 @@ public class AnswerRepositoryTests {
     private void createSampleData() {
         QuestionRepositoryTests.createSampleData(questionRepository);
 
+        // 관련 답변이 하나없는 상태에서 쿼리 발생
         Question q = questionRepository.findById(1).get();
 
         Answer a1 = new Answer();
@@ -44,11 +45,17 @@ public class AnswerRepositoryTests {
         a1.setCreateDate(LocalDateTime.now());
         answerRepository.save(a1);
 
+        q.getAnswerList().add(a1);
+
         Answer a2 = new Answer();
         a2.setContent("sbb에서는 주로 스프링부트관련 내용을 다룹니다.");
         a2.setQuestion(q);
         a2.setCreateDate(LocalDateTime.now());
         answerRepository.save(a2);
+
+        q.getAnswerList().add(a2);
+
+        questionRepository.save(q);
     }
 
     @Test
@@ -83,8 +90,6 @@ public class AnswerRepositoryTests {
         // SELECT * FROM question WHERE id = 1
         Question q = questionRepository.findById(1).get();
         // DB 연결이 끊김
-
-        System.out.println(q);
 
         // SELECT * FROM answer WHERE question_id = 1
         List<Answer> answerList = q.getAnswerList();
