f0dcc8fa49f1cffdba3a14f57c2d27b0da662dae
woominsik <9737638@naver.com>
2022-08-12 13:36:15 +0900

    git 결과까지 보여주기

--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -3,20 +3,16 @@ package com.ll.exam.sbb;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Entity
+@ToString
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,6 +26,6 @@ public class Question {
 
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
     private List<Answer> answerList;
 }
--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -63,5 +66,30 @@ public class AnswerRepositoryTests {
     void 조회() {
         Answer a = this.answerRepository.findById(1).get();
         assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판 입니다.");
+    }
+
+    @Test
+    void 관련된_question_조회() {
+        Answer a = this.answerRepository.findById(1).get();
+        Question q = a.getQuestion();
+
+        assertThat(q.getId()).isEqualTo(1);
+    }
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    void question으로부터_관련된_질문들_조회() {
+        // SELECT * FROM question WHERE id = 1
+        Question q = questionRepository.findById(1).get();
+        // DB 연결이 끊김
+
+        System.out.println(q);
+
+        // SELECT * FROM answer WHERE question_id = 1
+        List<Answer> answerList = q.getAnswerList();
+
+        assertThat(answerList.size()).isEqualTo(2);
+        assertThat(answerList.get(0).getContent()).isEqualTo("sbb는 질문답변 게시판 입니다.");
     }
 }
