aea5fba3d23ba00808820668640bb2f47410e7ff
woominsik <9737638@naver.com>
2022-08-12 14:42:33 +0900

    QuestionController 추가

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,21 +1,14 @@
 package com.ll.exam.sbb.question;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class QuestionController
-{
-    @Autowired
-    private QuestionService questionService;
-
-    @RequestMapping("list")
+public class QuestionController {
+    @RequestMapping("/question/list")
     @ResponseBody
-    public String showList() {
-        questionService.findById(1);
-
-        return "HI";
+    public String list() {
+        return "question list";
     }
 }
