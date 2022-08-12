8caf5983bb6887a4f0e0b76cd08af5b921fc78d8
woominsik <9737638@naver.com>
2022-08-12 14:34:33 +0900

    answer 를 저장하는 올바른 방법으로 변경

--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -24,4 +24,9 @@ public class Question {
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList = new ArrayList<>();
+
+    public void addAnswer(Answer answer) {
+        answer.setQuestion(this);
+        getAnswerList().add(answer);
+    }
 }
--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -41,41 +41,43 @@ public class AnswerRepositoryTests {
 
         Answer a1 = new Answer();
         a1.setContent("sbb는 질문답변 게시판 입니다.");
-        a1.setQuestion(q);
         a1.setCreateDate(LocalDateTime.now());
+        q.addAnswer(a1);
         answerRepository.save(a1);
-
-        q.getAnswerList().add(a1);
 
         Answer a2 = new Answer();
         a2.setContent("sbb에서는 주로 스프링부트관련 내용을 다룹니다.");
-        a2.setQuestion(q);
         a2.setCreateDate(LocalDateTime.now());
+        q.addAnswer(a2);
         answerRepository.save(a2);
-
-        q.getAnswerList().add(a2);
 
         questionRepository.save(q);
     }
 
     @Test
+    @Transactional
+    @Rollback(false)
     void 저장() {
         Question q = questionRepository.findById(2).get();
 
         Answer a = new Answer();
         a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q);
         a.setCreateDate(LocalDateTime.now());
+        q.addAnswer(a);
         answerRepository.save(a);
     }
 
     @Test
+    @Transactional
+    @Rollback(false)
     void 조회() {
         Answer a = this.answerRepository.findById(1).get();
         assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판 입니다.");
     }
 
     @Test
+    @Transactional
+    @Rollback(false)
     void 관련된_question_조회() {
         Answer a = this.answerRepository.findById(1).get();
         Question q = a.getQuestion();
@@ -87,11 +89,8 @@ public class AnswerRepositoryTests {
     @Transactional
     @Rollback(false)
     void question으로부터_관련된_질문들_조회() {
-        // SELECT * FROM question WHERE id = 1
         Question q = questionRepository.findById(1).get();
-        // DB 연결이 끊김
 
-        // SELECT * FROM answer WHERE question_id = 1
         List<Answer> answerList = q.getAnswerList();
 
         assertThat(answerList.size()).isEqualTo(2);
