68ace7d7769b3171e6a8ec7e265bff838856b5d6
woominsik <9737638@naver.com>
2022-08-16 14:37:02 +0900

    답변 저장 처리 및 답변 보여줌

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,16 +1,14 @@
 package com.ll.exam.sbb.question;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-import java.util.Optional;
 
+@RequestMapping("/question")
 @Controller
 @RequiredArgsConstructor // 생성자 주입
 // 컨트롤러는 Repository가 있는지 몰라야 한다.
@@ -24,7 +22,7 @@ public class QuestionController {
     // @Autowired // 필드 주입
     private final QuestionService questionService;
 
-    @RequestMapping("/question/list")
+    @RequestMapping("/list")
     // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
     public String list(Model model) {
         List<Question> questionList = questionService.getList();
@@ -36,10 +34,12 @@ public class QuestionController {
         return "question_list";
     }
 
-    @RequestMapping(value = "/question/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
-        Question question = this.questionService.getQuestion(id);
+    @RequestMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable int id) {
+        Question question = questionService.getQuestion(id);
+
         model.addAttribute("question", question);
+
         return "question_detail";
     }
 }
