48586c2788cf2a012884a0422fe0209401fd74f6
woominsik <9737638@naver.com>
2022-08-17 16:31:34 +0900

    한 페이지에서 10개씩 나오도록

--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -2,21 +2,22 @@ package com.ll.exam.sbb.question;
 
 import com.ll.exam.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.time.chrono.MinguoDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 10); // 한 페이지에 10까지 가능
+        return this.questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(int id) {
