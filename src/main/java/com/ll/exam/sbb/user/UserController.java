1e96847580f32574fe7fa03efbbe0052ed9d82ae
woominsik <9737638@naver.com>
2022-08-18 15:17:34 +0900

    상단바 회원가입 추가, 회원 username 중복 체크

--- src/main/java/com/ll/exam/sbb/user/UserController.java
+++ src/main/java/com/ll/exam/sbb/user/UserController.java
@@ -2,6 +2,7 @@ package com.ll.exam.sbb.user;
 
 import javax.validation.Valid;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,8 +35,18 @@ public class UserController {
             return "signup_form";
         }
 
-        userService.create(userCreateForm.getUsername(),
-                userCreateForm.getEmail(), userCreateForm.getPassword1());
+        try {
+            userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
 
         return "redirect:/";
     }
