7448a187ce1438e33a16746ff35dec44779d7afd
woominsik <9737638@naver.com>
2022-08-18 15:50:03 +0900

    회원가입시 중복관련 에러가 발생 했을 때, 정확히 어떠한 정보 때문에 문제가 되는건지 체크하여 오류 메시지에 명확히 기술

--- src/main/java/com/ll/exam/sbb/user/UserController.java
+++ src/main/java/com/ll/exam/sbb/user/UserController.java
@@ -38,13 +38,11 @@ public class UserController {
         try {
             userService.create(userCreateForm.getUsername(),
                     userCreateForm.getEmail(), userCreateForm.getPassword1());
-        } catch (DataIntegrityViolationException e) {
-            e.printStackTrace();
-            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+        } catch (SignupUsernameDuplicatedException e) {
+            bindingResult.reject("signupUsernameDuplicated", e.getMessage());
             return "signup_form";
-        } catch (Exception e) {
-            e.printStackTrace();
-            bindingResult.reject("signupFailed", e.getMessage());
+        } catch (SignupEmailDuplicatedException e) {
+            bindingResult.reject("SignupEmailDuplicated", e.getMessage());
             return "signup_form";
         }
 
--- src/main/java/com/ll/exam/sbb/user/UserService.java
+++ src/main/java/com/ll/exam/sbb/user/UserService.java
@@ -1,6 +1,7 @@
 package com.ll.exam.sbb.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,18 @@ public class UserService {
         user.setUsername(username);
         user.setEmail(email);
         user.setPassword(passwordEncoder.encode(password));
-        this.userRepository.save(user);
+
+        try {
+            userRepository.save(user);
+        }
+        catch (DataIntegrityViolationException e){
+            if(userRepository.existsByUsername(username)){
+                throw new SignupUsernameDuplicatedException("이미 등록된 아이디입니다.");
+            }
+            else{
+                throw new SignupEmailDuplicatedException("이미 등록된 이메일입니다.");
+            }
+        }
         return user;
     }
 }
