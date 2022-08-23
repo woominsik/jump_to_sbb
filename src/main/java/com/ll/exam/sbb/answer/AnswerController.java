0b55e082d6bf6693021715d06e187fb9a1d45cb2
woominsik <9737638@naver.com>
2022-08-23 11:45:26 +0900

    답변 수정폼 처리

--- src/main/java/com/ll/exam/sbb/answer/AnswerController.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -11,7 +11,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
@@ -30,7 +33,7 @@ public class AnswerController {
     public String detail(Principal principal, Model model, @PathVariable long id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
 
-        if ( bindingResult.hasErrors() ) {
+        if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             return "question_detail";
         }
@@ -49,7 +52,7 @@ public class AnswerController {
     public String answerModify(AnswerForm answerForm, @PathVariable("id") Long id, Principal principal) {
         Answer answer = answerService.getAnswer(id);
 
-        if ( answer == null ) {
+        if (answer == null) {
             throw new DataNotFoundException("데이터가 없습니다.");
         }
 
@@ -64,8 +67,19 @@ public class AnswerController {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/modify/{id}")
-    @ResponseBody
     public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult, @PathVariable("id") Long id, Principal principal) {
-        return answerForm.getContent();
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+
+        Answer answer = answerService.getAnswer(id);
+
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        answerService.modify(answer, answerForm.getContent());
+
+        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
     }
 }
